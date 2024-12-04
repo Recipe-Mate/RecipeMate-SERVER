9aa4080a96b241d851d895ceb71cc2d9b4402153
Choi Min Young <codingmy@naver.com>
2024-12-04 12:07:42 +0900

    [feat/#2] user 엔티티 추가

---
+++ src/main/java/org/chefcrew/user/entity/User.java
@@ -0,0 +1,24 @@
+package org.chefcrew.user.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Entity
+@Slf4j
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long userId;
+
+    private String userName;
+
+    private String email;
+}
