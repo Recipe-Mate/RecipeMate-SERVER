c82fdc531f25fd3171a69cce0f004807586cf65a
Choi Min Young <codingmy@naver.com>
2024-12-07 03:25:38 +0900

    [feat/#11] domain 추가

---
+++ src/main/java/org/chefcrew/recipe/domain/Recipe.java
@@ -0,0 +1,11 @@
+package org.chefcrew.recipe.domain;
+
+import java.util.List;
+
+public class Recipe {
+    private String recipeName;
+    private List<String> ingredient;
+    private List<String> cookingProcess;
+    private List<String> processImage;
+    private Float calorie;
+}
