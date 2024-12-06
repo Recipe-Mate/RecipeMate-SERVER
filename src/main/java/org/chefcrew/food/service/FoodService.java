e80b0492cc1f97d881ac481bbb2824ffee9d3f07
Choi Min Young <codingmy@naver.com>
2024-12-07 02:00:42 +0900

    [feat/#8] 식재료 조회 service 메서드 추가

--- src/main/java/org/chefcrew/food/service/FoodService.java
+++ src/main/java/org/chefcrew/food/service/FoodService.java
@@ -2,6 +2,7 @@ package org.chefcrew.food.service;
 
 import jakarta.transaction.Transactional;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.chefcrew.food.dto.request.FoodAddRequest;
 import org.chefcrew.food.entity.Food;
@@ -19,5 +20,15 @@ public class FoodService {
                 .map(name -> new Food(name, foodAddRequest.userId()))
                 .toList();
         foodRepository.saveFoodList(foodDataList);
+    }
+
+    public List<String> getOwnedFoodList(long userId) {
+        List<Food> foodList = foodRepository.findByUserId(userId);
+        if (foodList != null) {
+            return foodList.stream().map(food -> food.getFoodName())
+                    .collect(Collectors.toList());
+        } else {
+            return null;
+        }
     }
 }
