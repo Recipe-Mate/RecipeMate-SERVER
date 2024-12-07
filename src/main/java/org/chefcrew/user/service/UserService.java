2f0688d9a9079bc5b33e77650fc9cb4adb86a2f6
Choi Min Young <codingmy@naver.com>
2024-12-08 01:28:46 +0900

    [feat/#16] 유저 추가 service 로직 구현

--- src/main/java/org/chefcrew/user/service/UserService.java
+++ src/main/java/org/chefcrew/user/service/UserService.java
@@ -1,7 +1,11 @@
 package org.chefcrew.user.service;
 
+import static org.chefcrew.common.exception.ErrorException.ALREADY_EXIST_EMAIL;
+
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.chefcrew.common.exception.CustomException;
+import org.chefcrew.user.dto.request.LoginRequest;
 import org.chefcrew.user.dto.response.GetUserInfoResponse;
 import org.chefcrew.user.entity.User;
 import org.chefcrew.user.repository.UserRepository;
@@ -16,5 +20,16 @@ public class UserService {
     public GetUserInfoResponse getUserInfo(long userId) {
         User user = userRepository.findById(userId);
         return new GetUserInfoResponse(user.getUserId(), user.getUserName(), user.getEmail());
+    }
+
+    public void addUser(LoginRequest loginRequest){
+        if(isDuplicateEmail(loginRequest.email()))
+            throw new CustomException(ALREADY_EXIST_EMAIL);
+        User user = new User(loginRequest.email(), loginRequest.password(), loginRequest.userName());
+        userRepository.addUser(user);
+    }
+
+    public boolean isDuplicateEmail(String email){
+        return userRepository.existByEmail(email);
     }
 }
