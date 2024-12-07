48caabb7be24be99a4514d70652d7dc35687d65d
Choi Min Young <codingmy@naver.com>
2024-12-08 01:27:44 +0900

    [feat/#16] errorException 추가

--- src/main/java/org/chefcrew/common/exception/ErrorException.java
+++ src/main/java/org/chefcrew/common/exception/ErrorException.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorException {
 
-    REQUEST_NOT_FOUND(HttpStatus.BAD_REQUEST.value(), "잘못된 요청입니다.");
+    REQUEST_NOT_FOUND(HttpStatus.BAD_REQUEST.value(), "잘못된 요청입니다."),
+    ALREADY_EXIST_EMAIL(HttpStatus.BAD_REQUEST.value(), "이미 가입된 이메일입니다");
     int status;
     String errorMessage;
 
