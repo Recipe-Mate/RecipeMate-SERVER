e417c6b1109fcc6e1582f50ec560f16aaa2e7215
Choi Min Young <codingmy@naver.com>
2024-12-07 23:57:15 +0900

    [feat/#10] open api dto 추가

--- src/main/java/org/chefcrew/recipe/dto/request/GetRecipeRequest.java
+++ src/main/java/org/chefcrew/recipe/dto/request/GetRecipeRequest.java
@@ -6,6 +6,8 @@ public record GetRecipeRequest(
         String foodName,
         ValueOption calorie,        //칼로리
         ValueOption fat,            //지방
-        ValueOption sodium          //나트륨
+        ValueOption natrium,          //나트륨
+        ValueOption protien,            //단백질
+        ValueOption carbohydrate        //탄수화물
 ) {
 }
