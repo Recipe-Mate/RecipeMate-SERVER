cb911614a5e05c14315f3f76b180c44fa8cfc3e0
Choi Min Young <codingmy@naver.com>
2024-12-08 00:00:06 +0900

    [feat/#10] 레시피 조회 api 구현

---
+++ src/main/java/org/chefcrew/recipe/controller/RecipeController.java
@@ -0,0 +1,24 @@
+package org.chefcrew.recipe.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.chefcrew.recipe.dto.request.GetRecipeRequest;
+import org.chefcrew.recipe.dto.response.GetRecipeResponse;
+import org.chefcrew.recipe.service.RecipeService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/recipe")
+@RequiredArgsConstructor
+public class RecipeController {
+
+    private final RecipeService recipeService;
+
+    @GetMapping
+    public ResponseEntity<GetRecipeResponse> getRecommendRecipes(GetRecipeRequest requestBody){
+        return ResponseEntity.ok().body(recipeService.getRecommendRecipe(requestBody));
+    }
+
+}
