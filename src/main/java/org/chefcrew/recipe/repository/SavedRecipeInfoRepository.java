3cd09a2db45b6e8e4dbfe4698c7e45ae4390c3ba
Choi Min Young <codingmy@naver.com>
2025-03-21 21:39:56 +0900

    [feat/#28] 레시피 저장 메서드 추가

---
+++ src/main/java/org/chefcrew/recipe/repository/SavedRecipeInfoRepository.java
@@ -0,0 +1,35 @@
+package org.chefcrew.recipe.repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.chefcrew.recipe.entity.SavedRecipeInfo;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class SavedRecipeInfoRepository {
+    private final EntityManager em;
+    private final JPAQueryFactory query;
+
+    public SavedRecipeInfoRepository(EntityManager em) {
+        this.em = em;
+        this.query = new JPAQueryFactory(em);
+    }
+
+    public void saveRecipe(SavedRecipeInfo savedRecipeInfo) {
+        em.persist(savedRecipeInfo);
+    }
+
+    public boolean existsByRecipeName(String recipeName) {
+        Integer count = query.selectOne()
+                .from(savedRecipeInfo)
+                .where(savedRecipeInfo.recipeName.eq(recipeName))
+                .fetchFirst();
+        return count != null && count > 0;
+    }
+
+    public SavedRecipeInfo findByRecipeName(String recipeName) {
+        return query.selectFrom(savedRecipeInfo)
+                .where(savedRecipeInfo.recipeName.eq(recipeName))
+                .fetchFirst();
+    }
+}
