e2c77eea996cca1170f3016b6f83103e6695d6cb
Choi Min Young <codingmy@naver.com>
2025-03-21 19:40:52 +0900

    [feat/#28] SavedRecipeInfo 엔티티 추가

--- src/main/java/org/chefcrew/recipe/entity/SavedRecipeInfo.java
+++ src/main/java/org/chefcrew/recipe/entity/SavedRecipeInfo.java
@@ -1,11 +1,27 @@
 package org.chefcrew.recipe.entity;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
-public record SavedRecipeInfo(
-        long id,
-        long recipeId,
-        String recipeName,
-        String recipeImage
-) {
+@Entity
+@Slf4j
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class SavedRecipeInfo {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
 
+    String recipeName;
+
+    String recipeImage;
+
+    public SavedRecipeInfo(String recipeName, String recipeImage){
+        this.recipeName = recipeName;
+        this.recipeImage = recipeImage;
+    }
 }
