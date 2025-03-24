a9a9abfe2c8e4dc13df064e4c88cfa3395f4355b
Choi Min Young <codingmy@naver.com>
2025-03-25 01:39:16 +0900

    [feat/#28] 사용한 레시피 조회 api 구현

--- src/main/java/org/chefcrew/recipe/controller/RecipeController.java
+++ src/main/java/org/chefcrew/recipe/controller/RecipeController.java
@@ -3,14 +3,12 @@ package org.chefcrew.recipe.controller;
 import lombok.RequiredArgsConstructor;
 import org.chefcrew.recipe.Facade.RecipeFacade;
 import org.chefcrew.recipe.dto.request.GetRecipeRequest;
+import org.chefcrew.recipe.dto.request.GetUsedRecipeListRequest;
 import org.chefcrew.recipe.dto.request.PostUsedRecipeRequest;
 import org.chefcrew.recipe.dto.response.GetRecipeResponse;
 import org.chefcrew.recipe.service.RecipeService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/recipe")
@@ -30,4 +28,9 @@ public class RecipeController {
         return ResponseEntity.ok().build();
     }
 
+    @GetMapping("/used")
+    public ResponseEntity<Void> getUsedRecipeList(@RequestBody GetUsedRecipeListRequest requestBody) {
+        recipeFacade.getUsedRecipeList(requestBody);
+        return ResponseEntity.ok().build();
+    }
 }
