7efe52b149a2351fa616de62514e40bf40510290
Choi Min Young <codingmy@naver.com>
2025-04-04 04:39:11 +0900

    [feat/#30] CORS 세팅 추가

--- src/main/java/org/chefcrew/config/WebConfig.java
+++ src/main/java/org/chefcrew/config/WebConfig.java
@@ -4,13 +4,21 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @RequiredArgsConstructor
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
     private final UserIdResolver userIdResolver;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "DELETE")
+                .maxAge(3000);
+    }
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
