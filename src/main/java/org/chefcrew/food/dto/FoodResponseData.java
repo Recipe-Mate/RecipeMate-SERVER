8625b6ee6bf80e823fe4109665e6481bfd509242
Choi Min Young <codingmy@naver.com>
2025-04-15 02:31:40 +0900

    [feat/#36] 보유 식재료 조회 dto 수정

---
+++ src/main/java/org/chefcrew/food/dto/FoodResponseData.java
@@ -0,0 +1,11 @@
+package org.chefcrew.food.dto;
+
+import org.chefcrew.common.enums.AmountUnit;
+
+public class FoodResponseData {
+    private long foodId;
+    private String foodName;
+    private float amount;
+    private AmountUnit unit;
+    private String imgUrl;
+}
